a5b9c8ab865571c4b538359aaf4fdb65048efc91
deciek <luongvukhoa572001@gmail.com>
2023-11-21 23:40:33 +0700

    21

--- src/services/userService.js
+++ src/services/userService.js
@@ -1245,7 +1245,7 @@ let handleThemTTPhim = (data) => {
       if (
         !data.Tenphim ||
         !data.Dieukien ||
-        !data.Poster ||
+        // !data.Poster ||
         !data.Trailer ||
         !data.Dienvien ||
         !data.Ngonngu ||
